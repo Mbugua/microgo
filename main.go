e341d5177a38a693008cdcb29c9c454d3602bf60
mbugua <mbugua.dev@gmail.com>
2021-12-16 10:27:06 +0300

    update handlers and server

--- main.go
+++ main.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/mbugua/micro/handlers"
 )
 
 func main() {
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
 
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":8080", nil)
+	// http.ListenAndServe(":8000", sm)
+	s := &http.Server{
+		Addr:         ":8000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sch := make(chan os.Signal)
+	signal.Notify(sch, os.Interrupt)
+	signal.Notify(sch, os.Kill)
+	sig := <-sch
+
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	s.Shutdown(tc)
+
 }
